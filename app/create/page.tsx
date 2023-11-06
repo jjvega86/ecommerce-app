4107f7883798e5d41b9e063c31971aeb6dca9f6f
JJ <jj.vega86@gmail.com>
2023-11-06 10:49:08 -0600

    📦 NEW: Add create product page

---
+++ app/create/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const CreateProductPage = () => {
+  return (
+    <div>
+      <h1>Create a Product, bru</h1>
+      <Link href="/">Back to Products</Link>
+    </div>
+  )
+}
+
+export default CreateProductPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 
+import Link from "next/link";
 import ProductList from "./components/ProductList";
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-24">
+    <main className="min-h-screen p-24 flex flex-col">
       <h1 className="p-10 text-center">eCommerce App</h1>
-      <div>
-        
-      </div>
+      <Link className="mt-4 mb-8 w-40 self-center" href="/create">
+        <button className="bg-blue-500 text-white font-semibold px-4 py-2 rounded hover:bg-blue-600">
+          Add New Product
+        </button>
+      </Link>
       <ProductList />
     </main>
   )
